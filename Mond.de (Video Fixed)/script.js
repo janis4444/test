b0d32fe3958ed682b81902426dd58f4e2f58e656
janis <159477102+janis4444@users.noreply.github.com>
2024-02-08 20:34:10 +0100

    testing

---
+++ Mond.de (Video Fixed)/script.js
@@ -0,0 +1,11 @@
+
+
+// --- Footer --- 
+
+let mybutton = document.getElementById("myBtn");
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // Mac bzw Safari
+  document.documentElement.scrollTop = 0;
+}
